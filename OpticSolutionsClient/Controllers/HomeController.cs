7c0144b00e567685b7a19ac1c2d3ca2c90e16bd8
scm96steven <scm96steven@gmail.com>
2018-01-12 16:42:02 -0400

    Commit inicial

---
+++ OpticSolutionsClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpticSolutionsClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+      
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+         
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+        
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+       
+
+            return View();
+        }
+    }
+}
