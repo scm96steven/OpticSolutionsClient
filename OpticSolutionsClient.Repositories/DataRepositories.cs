205e1018c2c4ac88ee9e91e5c38a2b2c6b1e353c
Frederick Ramirez Luciano <frederick5@hotmail.com>
2018-01-13 15:37:48 -0800

    Repositories agregados

---
+++ OpticSolutionsClient.Repositories/DataRepositories.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using OpticSolutionsClient.Repositories.Entitys;
+
+namespace OpticSolutionsClient.Repositories
+{
+    public class DataRepositories
+    {
+        public SqlConnection conn = new SqlConnection();
+
+        public DataRepositories()
+        {
+            conn.ConnectionString = "Server=tcp:opticsolutions.database.windows.net,1433;Initial Catalog=OpticSolutions;Persist Security Info=False;User ID=osuser;Password=p@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+        }
+
+        public List<Doctor> GetDoctors()
+        {
+            conn.Open();
+            var data = conn.Query<Doctor>("GET_DOCTORS", null, commandType: System.Data.CommandType.StoredProcedure).ToList();
+            conn.Close();
+            return data;
+        }
+
+        public List<Appointment> GetAppointments(Appointment ap)
+        {
+            conn.Open();
+            var queryParameters = new DynamicParameters();
+            queryParameters.Add("@date", ap.Date);
+            queryParameters.Add("@doctor_username", ap.DoctorUsername);
+
+            var data = conn.Query<Appointment>("GET_APPOINTMENTS", queryParameters, commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+
+            conn.Close();
+            return data;
+        }
+    }
+}
---
+++ OpticSolutionsClient.Repositories/Entitys/Appointment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpticSolutionsClient.Repositories.Entitys
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string DoctorUsername { get; set; }
+        public string DoctorFullname { get; set; }
+
+        public int Status { get; set; }
+
+        [Required(ErrorMessage = "El campo nombres es requerido")]
+        public string Names { get; set; }
+
+        [Required(ErrorMessage = "El campo apellidos es requerido")]
+        public string Last_Names { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo telefono es requerido")]
+        public string Phone { get; set; }
+
+
+        public string IdentificationCard { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "El campo hora es requerido")]
+        public string StartHourStr { get; set; }
+
+        [Required(ErrorMessage = "El campo fecha es requerido")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-dd-mm}", ApplyFormatInEditMode = true)]
+        public string StartDateStr { get; set; }
+        public string EndDateStr { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int StartHour { get; set; }
+        public int StartMinute { get; set; }
+
+        public int EndHour { get; set; }
+        public int EndMinute { get; set; }
+
+
+
+
+        public int NumberOfAppointments { get; set; }
+    }
+}
